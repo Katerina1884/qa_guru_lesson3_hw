3afd1f21cfd7e21ff1a6e7b1b9e1d77d0ccc03da
Ekaterina Sushkova <sushkovakeit@yandex.ru>
2023-01-17 23:13:55 +0300

    finish

---
+++ src/test/java/AutomationFormTest.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class AutomationFormTest {
+    @Test
+    void fillFormTest() {
+        Configuration.browserSize = "1920x1080";
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Yulian");
+        $("#lastName").setValue("Sarychev");
+        $("#userEmail").setValue("mail@example.com");
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").setValue("7999123444");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("1989");
+        $(".react-datepicker__day--008").click();
+        $("#subjectsInput").setValue("Hindi").pressEnter();
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#hobbies-checkbox-2").parent().click();
+        $("#uploadPicture").uploadFromClasspath("img/111.jpg");
+        $("#currentAddress").setValue("Istra, Ivanova street, 98");
+        $("#state").click();
+        $("#react-select-3-option-3").click();
+        $("#city").click();
+        $("#react-select-4-option-0").click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Yulian"), text("Sarychev"), text("7999123444"));
+
+    }
+}
